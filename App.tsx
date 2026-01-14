76df198b01918ec84075dcfd4d940e7b5798cd16
egyirbransahprince-sys <egyirbransahprince@gmail.com>
2026-01-14 15:09:38 +0000

    Fix Firebase import

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useMemo, useRef, useEffect } from 'react';
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase";
+import { auth, db } from "./firebase";
 import { Teacher, Student, NawaClass, Gender } from './types';
 import Auth from './components/Auth';
 import { TeacherDashboard } from './components/TeacherDashboard';
